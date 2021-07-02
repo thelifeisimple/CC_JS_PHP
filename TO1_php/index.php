4757754ae1af0b77db3a62d471a299e1563b9bf3
thelifeisimple <email@example.com>
2021-07-01 21:59:20 -0300

    TO1_php

---
+++ TO1_php/index.php
@@ -0,0 +1,11 @@
+<?php
+$nombre = 'Ana';
+$apellido = ' Zuluaga';
+$edad= '32';
+$hobbie = 'Leer.'
+?>
+
+<h1> TO1 php 🖖</h1>
+
+<h3> Me llamo:</h3><?php echo ($nombre.$apellido)?><h3>Tengo: </h3><?php echo($edad)?>
+<h3>y en mis tiempos libres me gusta: </h3><?php echo($hobbie)?>
